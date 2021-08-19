55506b2a3467800236627234ed1d0acbbf03b9e8
valeriazamora <valezamora@gmail.com>
2021-08-18 22:53:06 -0600

    Clase 1

---
+++ Clase1/Clase1.java
@@ -0,0 +1,20 @@
+public class Clase1 {
+    public static void main(String[] args) {
+        // Saludo al grupo
+        String saludo = "Hola grupo 8 \nprogra I";
+        System.out.println(saludo);
+        /*
+        este es un comentario de bloque
+        tiene muchas lineas
+        ignoradas
+         */
+        /**
+         * esto tambien se ignora
+         */
+        saludo = "Hola grupo 9 progra I. ";
+        System.out.print(saludo);
+        System.out.println("Esta es la primera clase");
+
+        System.out.println();
+    }
+}
