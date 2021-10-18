225c3ffabdfa3ceaf2fec9c29f62279b4cb5e6f8
valeriazamora <valezamora@gmail.com>
2021-10-18 08:29:05 -0600

    Ejemplo aproximacion Pi para resolver

---
+++ Clase16/Calculadora.java
@@ -0,0 +1,43 @@
+public class Calculadora {
+
+    
+    private double signo(long t ){
+       return Math.pow(-1,t+1);
+    }
+    
+    private double denominador( long t){
+       return 2.0 * t - 1.0;
+    }
+    public double pi00(long t){
+       double aproximacionDePi = 0;
+       for(long i = 1; i <= t; ++i){
+          aproximacionDePi+= signo( i ) * 4.0 / denominador( i ); 
+       } 
+       
+       return aproximacionDePi;
+    }
+
+    public double pi01(long t){
+       double aproximacionDePi = 0;
+       double termino = 0;
+       double denominador = 1.0;
+       boolean suma = true;
+       for( long i = 0; i < t; ++i){
+          termino = 4.0 / denominador;
+          if(suma){
+             aproximacionDePi+= termino;  
+          }
+          else {
+             aproximacionDePi-= termino;  
+          }
+          suma = !suma;
+          denominador+=2.0;
+       }
+       return aproximacionDePi;
+    } 
+    
+    public double pi02(long t){
+       double aproximacionDePi = 0;
+       return aproximacionDePi;
+    }    
+}
